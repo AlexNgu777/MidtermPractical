22bc7ea64ce856679a76e1eb58dce7524631746d
AlexNgu777 <alexnguyen@Alexs-MacBook-Pro-4.local>
2024-10-17 08:49:29 -0400

    began working on logic for problems

--- app/src/main/java/sheridancollege/ngualexa/midtermpractical/data/MathProblems.kt
+++ app/src/main/java/sheridancollege/ngualexa/midtermpractical/data/MathProblems.kt
@@ -1,4 +1,24 @@
 package sheridancollege.ngualexa.midtermpractical.data
 
+import kotlin.random.Random
+
 var operations = listOf<String>("*","+","-")
-var numbers = listOf<Int>(69,72,48,31,10,25,83)
+var questions = mutableListOf<String>()
+var correctAnswers = mutableListOf<String>()
+var wrongAnswers = mutableListOf<String>()
+
+
+fun mathProblems(): List<String> {
+    for (i in 0..4) {
+        var number1 = Random.nextInt(1,100)
+        var number2 = Random.nextInt(1,100)
+        var operation = operations.random()
+        var question = "$number1 $operation $number2"
+        questions[i] = question
+    }
+    return questions
+}
+
+fun checkAnswers(questions: List<String>, answers: List<String>) {
+    
+}
