81df55deac9316a4e8a00b78ac5ac4559e8b6d6a
AlexNgu777 <alexnguyen@Alexs-MacBook-Pro-4.local>
2024-10-17 09:14:38 -0400

    created the operations row

--- app/src/main/java/sheridancollege/ngualexa/midtermpractical/components/MathOperations.kt
+++ app/src/main/java/sheridancollege/ngualexa/midtermpractical/components/MathOperations.kt
@@ -13,42 +13,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 
-@Composable
-fun MathOperations() {
-    var answer1 by remember { mutableStateOf(TextFieldValue("")) }
-    var answer2 by remember { mutableStateOf(TextFieldValue("")) }
-    var answer3 by remember { mutableStateOf(TextFieldValue("")) }
-    var answer4 by remember { mutableStateOf(TextFieldValue("")) }
 
-    Column() {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Text(text = "")
-            Text(text = "")
-            Text(text = "")
-            TextField(value = answer1,
-                onValueChange = {answer1 = it})
-        }
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Text(text = "")
-            Text(text = "")
-            Text(text = "")
-            TextField(value = answer2,
-                onValueChange = {answer2 = it})
-        }
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Text(text = "")
-            Text(text = "")
-            Text(text = "")
-            TextField(value = answer3,
-                onValueChange = {answer3 = it})
-        }
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Text(text = "")
-            Text(text = "")
-            Text(text = "")
-            TextField(value = answer4,
-                onValueChange = {answer4 = it})
+
+@Composable
+fun MathProblemsSection(questions: List<String>, answers: MutableList<String>) {
+    Column {
+        questions.forEachIndexed { index, question ->
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(text = question)
+                UserAnswerField(
+                    answer = questions[index],
+                    onAnswerChange = { answers[index] = it }
+                )
+            }
         }
     }
-
 }
--- app/src/main/java/sheridancollege/ngualexa/midtermpractical/components/UserAnswerField.kt
+++ app/src/main/java/sheridancollege/ngualexa/midtermpractical/components/UserAnswerField.kt
@@ -1,2 +1,13 @@
 package sheridancollege.ngualexa.midtermpractical.components
 
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+
+@Composable
+fun UserAnswerField(answer: String, onAnswerChange: (String) -> Unit) {
+    TextField(
+        value = answer,
+        onValueChange = onAnswerChange,
+    )
+}
